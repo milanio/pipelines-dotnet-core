d7fd8c7d2f99dc6ceb5f654546d1c6e960abc723
milanio <milan.krizalkovic@gmail.com>
2021-06-23 12:32:06 +0100

    Quick update for net 5

--- Startup.cs
+++ Startup.cs
@@ -31,8 +31,9 @@ namespace pipelines_dotnet_core
                 options.MinimumSameSitePolicy = SameSiteMode.None;
             });
 
-
-            services.AddMvc().SetCompatibilityVersion(CompatibilityVersion.Version_2_2);
+            services.AddMvc(a=> {
+                a.EnableEndpointRouting = false;
+            }).SetCompatibilityVersion(CompatibilityVersion.Version_2_2);
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
